6f8a470c0c3d9dee6756bd74629e12b83b79058e
V. Naydov <111379967+V-NaydoV@users.noreply.github.com>
2023-05-17 14:21:46 +0300

    Create supermarket.py

---
+++ supermarket.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+command = input()
+custemours = deque([])
+
+while command != 'End':
+    if command == "Paid":
+        while custemours:
+            print(custemours.popleft())
+    else:
+        custemours.append(command)
+    command = input()
+
+print(f"{len(custemours)} people remaining.")
+
