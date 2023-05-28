f7b5a219a6748f96f07b765b5cada370dac5d4c5
V. Naydov <111379967+V-NaydoV@users.noreply.github.com>
2023-05-28 12:52:20 +0300

    2 solutions

---
+++ Tuples and Sets - Exercise/04.count_symbols.py
@@ -0,0 +1,19 @@
+# text = input()
+# for letter in sorted(set(text)):
+#     print(f"{letter}: {text.count(letter)} time/s")
+
+
+
+
+
+#text = input()
+#
+#letters = {}
+#
+#for letter in text:
+#    if letter not in letters:
+#        letters[letter] = 0
+#    letters[letter] += 1
+#
+#for let, rep_times in sorted(letters.items()):
+#    print(f"{let}: {rep_times} time/s")
